54d6e9cd60111a6cfd7565a26c5cbaeb2f34065f
Lipich <li98pen15sa09sha@gmail.com>
2020-02-28 09:00:05 +0300

    Updates

---
+++ precache-manifest.435a0bb9d2681c82b95e5409c5af3435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5419d90ec3d038249bea0b9364482c96",
+    "url": "/Country/index.html"
+  },
+  {
+    "revision": "35bf876d7af207cbfc1a",
+    "url": "/Country/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "c9664d1f6f9fa7e6ae9b",
+    "url": "/Country/static/css/main.65d5060c.chunk.css"
+  },
+  {
+    "revision": "35bf876d7af207cbfc1a",
+    "url": "/Country/static/js/2.f02dc0a2.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Country/static/js/2.f02dc0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9664d1f6f9fa7e6ae9b",
+    "url": "/Country/static/js/main.e168769d.chunk.js"
+  },
+  {
+    "revision": "7c1cf58bf3772dfd6e9f",
+    "url": "/Country/static/js/runtime-main.16cf9fca.js"
+  }
+]);
